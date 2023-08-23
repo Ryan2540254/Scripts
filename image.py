d6d93b7d417ae7489aa8be96925d2a4999723254
ryangacheru <ryangacheru@gmail.com>
2023-08-23 14:41:14 +0300

    Added extra functions.

--- image.py
+++ image.py
@@ -6,4 +6,27 @@ from PIL import Image
 """Python Script that Shows the various uses of the PIL module."""
 name = input('Enter the name of the Image:')
 im   = Image.open(name)
-im.rotate(45).show
+
+
+def rotate_image():
+    new_im   = im.rotate(45)
+    new_name = input('Enter the new name of the Image:')
+    new_im.save(new_name)
+def resize_image():
+    new_imm  = im.resize((640,480))
+    new_name = input('Enter the new name of the Image:')
+    new_im.save(new_name)
+def rotate_and_resize_image():
+    new_name = input('Enter the new name of the Image:')
+    im.rotate(180).resize((640,480)).save(new_name)
+
+print('Current options:rotate,resize,both')
+ans = input('Which operation would you like to do on the image:')
+if ans.lower() == 'rotate':
+   rotate_image()
+elif ans.lower() == 'resize':
+   resize_image()
+elif ans.lower() == 'both':
+   rotate_and_resize_image()
+else:
+    print('Enter a valid input')
