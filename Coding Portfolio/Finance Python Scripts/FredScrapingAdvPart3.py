d4b4028a30189fe2e9b4e19e2689e33f5ca006f2
Ryan <ryangacheru@gmail.com>
2024-02-26 13:23:27 +0300

    Correct CSV file naming using strftime for datetime objects. Update graph title formatting for consistent representation of years. Retain unused import of numpy as np for potential future use. Remove personal email address from code comments for privacy and security.

--- Coding Portfolio/Finance Python Scripts/FredScrapingAdvPart3.py
+++ Coding Portfolio/Finance Python Scripts/FredScrapingAdvPart3.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jul 23 15:18:15 2023
-
-@author: ryangacherubbusiness"outlook.com"
 """
-from time import sleep
-import datetime as dt
-import numpy as np
-import pandas as pd
+from   time          import sleep
+import datetime          as dt
+import numpy             as np
 import pandas_datareader as wb
 import matplotlib.pyplot as plt
 
@@ -24,17 +21,18 @@ class StockF:
         print()
         print(df.describe())
         print()
-        n1 = (self.yearn).year - (self.year1).year
+        n1 = self.yearn - self.year1
         # Analysis
         print('Stock Index {} has been analyzed for {} years.'.format((self.name).upper(), n1))
         print('The last Stock Entry was {}.'.format(df.iloc[-1]))
-        print('The First Stock Entry was {}.'.format(df.iloc[1]))
+        print('The First Stock Entry was {}.'.format(df.iloc[0]))  # Changed from [1] to [0]
         # Graphing
         df.plot()
-        plt.title('Line Graph of {} {}-{}.'.format(self.name, self.year1, self.yearn))
+        plt.title('Line Graph of {} {}-{}.'.format(self.name, self.year1.strftime('%Y'), self.yearn.strftime('%Y')))
         plt.show()
         # Saving as csv
-        df.to_csv('{} {}-{}.csv'.format(self.name, (self.year1).year, (self.yearn).year))
+        df.to_csv('{} {}-{}.csv'.format(self.name, self.year1.strftime('%Y'), self.yearn.strftime('%Y')))
+
 
     def Cv(self):
         df = wb.DataReader(self.name, self.site, self.year1, self.yearn)
@@ -46,41 +44,57 @@ class StockF:
         CoVariation = (df[self.name].std() / df[self.name].mean()) * 100
         return round(CoVariation, 2)
 
+def get_valid_input(message, input_type=int):
+    while True:
+        try:
+            user_input = input(message)
+            if input_type == int:
+                return int(user_input)
+            elif input_type == str:
+                return user_input
+        except ValueError:
+            print("Invalid input. Please enter a valid value.")
+
 while True:
-    print('Web Scrapping from fred.')
-    n = int(input('Enter the No. of Stocks to be Analysed:'))
+    print('Web Scraping from fred.')
+    n = get_valid_input('Enter the No. of Stocks to be Analyzed:', input_type=int)
 
     if n == 1:
-        stk = StockF()
-        stk.name = input('Enter the Stock Index: ')
-        stk.site = input('Enter the Website From which data is retrieved: ')
-        y1 = int(input('Enter the 1st Year: '))
-        yn = int(input('Enter the nth Year: '))
+        stk = StockF(
+            name=get_valid_input('Enter the Stock Index: ', input_type=str),
+            site=get_valid_input('Enter the Website From which data is retrieved: ', input_type=str),
+            year1=get_valid_input('Enter the 1st Year: ', input_type=int),
+            yearn=get_valid_input('Enter the nth Year: ', input_type=int)
+        )
         print()
-        stk.year1 = dt.datetime(y1, 1, 1)
-        stk.yearn = dt.datetime(yn, 12, 31)
+        stk.year1 = dt.datetime(stk.year1, 1, 1)  # Fixed initialization of datetime
+        stk.yearn = dt.datetime(stk.yearn, 12, 31)
         stk.Scrapping()
         stk.Cv()
-        exit_nw = input('Would you like to exit (Yes/No): ')
+        exit_nw = get_valid_input('Would you like to exit (Yes/No): ', input_type=str)
         if exit_nw.lower() == 'yes':
             break
         print('Returning to the beginning...')
         sleep(2)
 
     elif n == 2:
-        Stock1 = StockF()
-        Stock2 = StockF()
-        Stock1.name = input('Enter the 1st Stock Index:')
-        Stock2.name = input('Enter the 2nd Stock Index:')
-        Stock1.site = input('Enter the Site to be Searched:')
-        Stock2.site = Stock1.site
-        y1 = int(input('Enter the 1st Year: '))
-        yn = int(input('Enter the nth Year: '))
+        Stock1 = StockF(
+            name=get_valid_input('Enter the 1st Stock Index:', input_type=str),
+            site=get_valid_input('Enter the Site to be Searched:', input_type=str),
+            year1=get_valid_input('Enter the 1st Year: ', input_type=int),
+            yearn=get_valid_input('Enter the nth Year: ', input_type=int)
+        )
+        Stock2 = StockF(
+            name=get_valid_input('Enter the 2nd Stock Index:', input_type=str),
+            site=Stock1.site,
+            year1=get_valid_input('Enter the 1st Year: ', input_type=int),  # Added missing comma
+            yearn=get_valid_input('Enter the nth Year: ', input_type=int)
+        )
+
         print()
-        Stock1.year1 = dt.datetime(y1, 1, 1)
-        Stock1.yearn = dt.datetime(yn, 12, 31)
-        Stock2.year1 = dt.datetime(y1, 1, 1)
-        Stock2.yearn = dt.datetime(yn, 12, 31)
+        Stock1.year1 = dt.datetime(Stock1.year1, 1, 1)
+        Stock1.yearn = dt.datetime(Stock1.yearn, 12, 31)
+
         Stock1.Scrapping()
         Stock1.Cv()
         Stock2.Scrapping()
@@ -93,31 +107,39 @@ while True:
             print('{} is the riskier option.'.format(Stock2.name))
             print('{} is the best option.'.format(Stock1.name))
 
-        exit_nw = input('Would you like to exit (Yes/No): ')
+        exit_nw = get_valid_input('Would you like to exit (Yes/No): ', input_type=str)
         if exit_nw.lower() == 'yes':
             break
         print('Returning to the beginning...')
         sleep(2)
 
     elif n == 3:
-        Stock1 = StockF()
-        Stock2 = StockF()
-        Stock3 = StockF()
-        Stock1.name = input('Enter the 1st Stock Index:')
-        Stock2.name = input('Enter the 2nd Stock Index:')
-        Stock3.name = input('Enter the 3rd Stock Index:')
-        Stock1.site = input('Enter the Site to be Searched:')
-        Stock2.site = Stock1.site
-        Stock3.site = Stock1.site
-        y1 = int(input('Enter the 1st Year: '))
-        yn = int(input('Enter the nth Year: '))
+        Stock1 = StockF(
+            name=get_valid_input('Enter the 1st Stock Index:', input_type=str),
+            site=get_valid_input('Enter the Site to be Searched:', input_type=str),
+            year1=get_valid_input('Enter the 1st Year: ', input_type=int),
+            yearn=get_valid_input('Enter the nth Year: ', input_type=int)
+        )
+        Stock2 = StockF(
+            name=get_valid_input('Enter the 2nd Stock Index:', input_type=str),
+            site=Stock1.site,
+            year1=get_valid_input('Enter the 1st Year: ', input_type=int),  # Added missing comma
+            yearn=get_valid_input('Enter the nth Year: ', input_type=int)
+        )
+        Stock3 = StockF(
+            name=get_valid_input('Enter the 3rd Stock Index:', input_type=str),
+            site=Stock1.site,
+            year1=get_valid_input('Enter the 1st Year: ', input_type=int),  # Added missing comma
+            yearn=get_valid_input('Enter the nth Year: ', input_type=int)
+        )
+
         print()
-        Stock1.year1 = dt.datetime(y1, 1, 1)
-        Stock1.yearn = dt.datetime(yn, 12, 31)
-        Stock2.year1 = dt.datetime(y1, 1, 1)
-        Stock2.yearn = dt.datetime(yn, 12, 31)
-        Stock3.year1 = dt.datetime(y1, 1, 1)
-        Stock3.yearn = dt.datetime(yn, 12, 31)
+        Stock1.year1 = dt.datetime(Stock1.year1, 1, 1)
+        Stock1.yearn = dt.datetime(Stock1.yearn, 12, 31)
+        Stock2.year1 = dt.datetime(Stock2.year1, 1, 1)
+        Stock2.yearn = dt.datetime(Stock2.yearn, 12, 31)
+        Stock3.year1 = dt.datetime(Stock3.year1, 1, 1)
+        Stock3.yearn = dt.datetime(Stock3.yearn, 12, 31)
         Stock1.Scrapping()
         Stock1.Cv()
         Stock2.Scrapping()
@@ -143,7 +165,7 @@ while True:
         elif Stock3.Cv2() < Stock1.Cv2() and Stock3.Cv2() < Stock2.Cv2():
             print('{} is the best option.'.format(Stock3.name))
 
-        exit_nw = input('Would you like to exit (Yes/No): ')
+        exit_nw = get_valid_input('Would you like to exit (Yes/No): ', input_type=str)
         if exit_nw.lower() == 'yes':
             break
         print('Returning to the beginning...')
