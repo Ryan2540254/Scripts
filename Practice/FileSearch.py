f24f01b3f3261107b2633f19b2be86f01cd74586
Ryan <ryangacheru@gmail.com>
2024-02-26 12:37:29 +0300

    Updated changes and typos.

---
+++ Practice/FileSearch.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+path   = input('Enter the path of the file:')
+
+
+if os.path.exists(path):
+    if os.path.isdir(path):
+        folder = os.listdir(path)
+        for file in folder:
+             print(file)
+else:
+    print(f"{path} isn't a valid drirectory")
+
