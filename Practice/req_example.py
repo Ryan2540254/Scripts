92bd20baba37f83aff301f793e2f92ec72ec4c1d
Ryan <ryangacheru@gmail.com>
2024-04-20 18:25:03 +0300

    "Removed unnecessary folder."

--- Practice/req_example.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import requests
-url      = input('Enter the url:')
-response = requests.get(url)
-if response.ok:
-   """Will print True if response was good & False if not."""
-   print(response.text[:150])
-   response.raise_for_status()
